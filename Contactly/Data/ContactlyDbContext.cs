f0386bafe4a56428f3c44c436c905a7f9cfd0e32
Muhammad Aiman <aiman@agmo.group>
2024-07-17 17:57:10 +0800

    new commits

---
+++ Contactly/Data/ContactlyDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Contactly.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Principal;
+
+namespace Contactly.Data
+{
+    public class ContactlyDbContext: DbContext
+    {
+        public ContactlyDbContext(DbContextOptions options) : base(options) {
+
+        
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
